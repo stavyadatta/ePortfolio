3058cb160f0d23514abb98ab73794d0f8f01f38f
Stavya Datta <stavyad@student.unimelb.edu.au>
2020-09-20 22:41:44 +0530

    Changes before frontend

--- functions/routers/user.routes.js
+++ functions/routers/user.routes.js
@@ -10,22 +10,22 @@ const router = new express.Router()
 router.post('/user/add', (req, res) => {
     var x = userController.addUser(req.body)
     res.send("Added")
-  })
-  
+})
+
 router.post('/user/update', (req, res) => {
     userController.updateUser(req.body).then((response) => {
         console.log(response)
         return res.send("Updated")
     }).catch((error) => {
-      console.log(error)
+        console.log(error)
     })
-  })
-  
+})
+
 router.post('/user/delete', (req, res) => {
     userController.deleteUser(req.body).then((response) => {
         return res.send(response)
     }).catch((error) => {
-      console.log(error)
+        console.log(error)
     })
-  })
+})
 module.exports = router
