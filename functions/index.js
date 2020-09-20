b3e39f82c7dddbd4611c087ce9926a978b7e4243
Stavya Datta <stavyad@student.unimelb.edu.au>
2020-09-20 22:27:32 +0530

    Added user routes file

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,11 @@
 const functions = require('firebase-functions');
-const firebase = require('firebase-admin')
-const engines = require("consolidate")
-const { response } = require('express');
 const express = require('express')
-const userController = require('./controllers/userController.js')
+const userRouter = require('./routers/user.routes.js')
 const port = process.env.PORT || 3000
 
 const app = new express()
 app.use(express.json())
-
-app.post('/user/add', (req, res) => {
-  var x = userController.addUser(req.body)
-  res.send("Added")
-})
-
-app.post('/user/update', (req, res) => {
-  userController.updateUser(req.body).then((response) => {
-      console.log(response)
-      return res.send("Updated")
-  }).catch((error) => {
-    console.log(error)
-  })
-})
-
-app.post('/user/delete', (req, res) => {
-  userController.deleteUser(req.body).then((response) => {
-      return res.send(response)
-  }).catch((error) => {
-    console.log(error)
-  })
-})
+app.use(userRouter)
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
---
+++ functions/routers/user.routes.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const userController = require('../controllers/userController.js')
+const functions = require('firebase-functions');
+const firebase = require('firebase-admin')
+const { response } = require('express');
+
+const port = process.env.PORT || 3000
+const router = new express.Router()
+
+router.post('/user/add', (req, res) => {
+    var x = userController.addUser(req.body)
+    res.send("Added")
+  })
+  
+router.post('/user/update', (req, res) => {
+    userController.updateUser(req.body).then((response) => {
+        console.log(response)
+        return res.send("Updated")
+    }).catch((error) => {
+      console.log(error)
+    })
+  })
+  
+router.post('/user/delete', (req, res) => {
+    userController.deleteUser(req.body).then((response) => {
+        return res.send(response)
+    }).catch((error) => {
+      console.log(error)
+    })
+  })
+module.exports = router
