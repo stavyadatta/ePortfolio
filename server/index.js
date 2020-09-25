d0188c9b61294a8fb9f2e1780aba12041f5a406c
Ujjwal071997 <uagarwal@student.unimelb.edu.au>
2020-09-25 12:59:22 +1000

    fixed authentication code

--- server/index.js
+++ server/index.js
@@ -1,50 +1,23 @@
-<<<<<<< HEAD
 const express = require('express');
+const bodyParser = require('body-parser');
+var path = require('path');
 const app = new express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
+// use the body-parser middleware, which parses request bodies into req.body
+// support parsing of json
+app.use(bodyParser.json());
+// support parsing of urlencoded bodies (e.g. for forms)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
 	res.render('index.html');
 });
-=======
-const express = require('express')
-const userDB = require('./db/userDB.js')
-const app = new express()
-const port = process.env.PORT || 3000
-
-app.use(express.json())
-
-app.post('/user/add', (req, res) => {
-    userDB.addUser(req.body, "test3").then((user) => {
-        res.send("Added")
-    }).catch((err) => {
-        res.send(err)
-    })
-})
-
-app.post('/user/update', (req, res) => {
-    userDB.updateUser(req.body, "random").then((user) => {
-        res.send("Updated")
-    }).catch((err) => {
-        res.send(err)
-    })
-
-})
-
-
-app.listen(port, () => {
-    console.log("listening on " + port)
-})
-//const testObj = {password: 886094}
-
-
-//userDB.updatingData(testObj, 'Stavya')
->>>>>>> ced119aed601001b496966f3f7af612b791bcbf4
 
 app.listen(port, () => {
 	console.log('listening on ' + port);
 });
+app.use(express.static(path.join(__dirname, 'scripts')));
