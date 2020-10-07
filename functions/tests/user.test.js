76f7790a1415202dc73592f773a603148f12d576
Stavya Datta <stavyad@student.unimelb.edu.au>
2020-10-07 16:34:29 +0530

    Add test files

---
+++ functions/tests/user.test.js
@@ -0,0 +1,7 @@
+const { testLab } = require("firebase-functions");
+
+// At the top of test/index.test.js
+const {add} = require('../routes/user.routes.js')
+test('testing add function', async () => {
+    
+})
