072085e9e713e2d6833bcf9967a37a8131fee48f
Farhan Sajeed <fsajeed@student.unimelb.edu.au>
2020-10-12 18:34:38 +1100

    Adjusted some css

--- Front-End/everest/src/Generic_Components/Project.js
+++ Front-End/everest/src/Generic_Components/Project.js
@@ -20,6 +20,16 @@ function Project(props) {
 
 export default Project;
 
+// ,
+// {
+//     "id": "4",
+//     "name": "Project_4",
+//     "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+//     "imgURL":"https://project-management.com/wp-content/uploads/2016/11/prepare-for-business-project.jpg",
+//     "phone": "+918 372 574"
+// }
+
+
 // function Project(props) {
 //     return (
 //       <div className="section1">
