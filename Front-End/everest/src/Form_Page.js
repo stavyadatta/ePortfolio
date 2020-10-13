759c0594d6681c975a486911e6cc74b2d1d6d02e
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-10-13 16:56:21 +1100

    Restore Form_Page to use form

--- Front-End/everest/src/Form_Page.js
+++ Front-End/everest/src/Form_Page.js
@@ -1,18 +1,16 @@
 import React, { useState } from "react";
 import "./Form_Page.css";
 import { withRouter } from "react-router-dom";
-import EntryBox from "./Generic_Components/Entry_Box";
-
-import firebase from "./Firebase"
 
 function FormPage(props) {
   const [pName, setPName] = useState("");
   const [pDesc, setPDesc] = useState("");
   const [pBody, setPBody] = useState("");
   const [pTags, setPTags] = useState("");
-  //const [pImg, setPImg] = useState("");
+  const [pImg, setPImg] = useState("");
 
   const updateField = (e) => {
+      console.log(pName, pDesc, pBody, pTags);
     let fieldValue = e.target.value;
     if (e.target.id === "name_entry") {
         setPName(fieldValue);
@@ -26,25 +24,16 @@ function FormPage(props) {
     if (e.target.id === "tags_entry") {
           setPTags(fieldValue);
         }
-    if (e.target.id === "myFile") {
-            //setPImg(fieldValue);
-          }
-    
 
+    if (e.target.id === "myFile") {
+            setPImg(fieldValue);
+          }
     return;
   };
 
-  const submit = () => {
-    console.log(pName, pDesc, pBody, pTags);
-    firebase.functions().httpsCallable('project-add')({
-      projectName:pName, 
-      projectDesc:pDesc, 
-      projectBody:pBody, 
-      projectTags:pTags.split(','),
-      userId:firebase.auth().currentUser.uid
-    }).then(res=>console.log(res)).catch(e=>console.error(e));
+  const submitHandler = () => {
    
-    //props.history.push({pathname:"/addproject",state:{pName:pName, pDesc: pDesc, pBody: pBody, pTags:pTags, PImg: pImg }}); /* CHANGHES TO PROJECT PAGE (WITH DETAILS) WHEN CLICKED ON SAVE PROJECT */
+    props.history.push({pathname:"/addproject",state:{pName:pName, pDesc: pDesc, pBody: pBody, pTags:pTags, PImg: pImg }}); /* CHANGHES TO PROJECT PAGE (WITH DETAILS) WHEN CLICKED ON SAVE PROJECT */
   };
 
   return (
@@ -54,21 +43,31 @@ function FormPage(props) {
 
           {/************************************************************************/}
             <div className = "mt-5 column form-box">
-            <EntryBox id = "name_entry" textType = "text" 
-    	  	  default = "Project name"
-        	  onChange = {updateField} />
-            <EntryBox id = "desc_entry" textType = "text" 
-    	  	  default = "Project description"
-        	  onChange = {updateField} />
-            <EntryBox id = "body_entry" textType = "text" 
-    	  	  default = "Project body"
-        	  onChange = {updateField} />
-            <EntryBox id = "tags_entry" textType = "text" 
-    	  	  default = "Project tags"
-        	  onChange = {updateField} />
-            <button onClick={submit}>Submit</button>
-              
-            
+            <form className = "form_fields">
+                <div class="form-group">
+                   
+                    <input className="form-control" type="text" onChange={updateField}  id="name_entry" placeholder="Enter Project Name"/>
+                   
+                </div>
+                <div className="form-group">
+                    
+                    <input className="form-control" type="text" onChange={updateField}  id="desc_entry" placeholder="Enter Project Description" />
+                </div>
+                <div className="form-group">
+                   
+                    <input className="form-control" type="text" onChange={updateField}  id="body_entry"  placeholder="Enter Detailed Body of Project"/>
+                </div>
+                <div className="form-group">
+                    
+                    <input className="form-control" type="text" onChange={updateField}  id="tags_entry"  placeholder="Enter Project Tags"/>
+                </div>
+                <div className="form-group">
+                    
+                    <input className="form-control" type="file" id="myFile" name="filename" onChange={(event)=> {console.log(event.target.value)}}/>
+                </div>
+                
+                <button style = {{backgroundColor:"#082f4e"}} type="submit" className="mt-4 btn btn-primary" onClick={submitHandler}>Submit</button>
+            </form>
            
           </div>
 
