301bfb020345f72288bb39ed712e15d88dec77e8
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-10-15 10:26:46 +1100

    Add projectDetails

--- Front-End/everest/src/Project_Details_Page.js
+++ Front-End/everest/src/Project_Details_Page.js
@@ -7,9 +7,16 @@ import "./Project_Details_Page.css";
 
 function ProjectDetailsPage(props){
     let project = props.project;
+    let details = props.projectDetails
 
+    //check if data is loaded
+    if(!project || !details){
+        return(
+            <div>Loading...</div>
+        )
+    }
     return (
-        <projectDetails>
+        <div className="projectLayout">
             <div className="projectHeader">
                 <div className="projectTitle">{project.projectName}</div>
             </div>
@@ -17,26 +24,63 @@ function ProjectDetailsPage(props){
                 <div className="descriptionTitle">Description</div>
                 <div className="descriptionBody">{project.projectDesc}</div>
             </div>
-            <div className="projectContent">
-                <div className="contentBody">{project.projectBody}</div>
-            </div>
-        </projectDetails>
+            <ProjectDetailList details={details}/>
+            
+        </div>
     );
+}
+
+const ProjectDetailList = (props) => {
+    return (props.details.map((detail)=>(<ProjectDetail detail={detail}></ProjectDetail>)))
+}
+
+const ProjectDetail = (props)=>{
+    let detail=props.detail;
+    switch(detail.type){
+        case 'right-image':
+            return(
+                <div className="projectContent">
+                    <div className="halfDetailText">{props.detail.text}</div>
+                    <div className="detailImageWrap">
+                        <img className="detailImage" alt={detail.imageText} src={detail.imageUrl}/>
+                    </div>                
+                </div>
+            )
+        case 'left-image':
+            return(
+                <div className="projectContent">
+                    <div className="detailImageWrap">
+                        <img className="detailImage" alt={detail.imageText} src={detail.imageUrl}/>
+                    </div>
+                    <div className="halfDetailText">{detail.text}</div>
+                </div>
+            )
+        default:
+            return(
+                <div className="projectContent">
+                    <div className="detailText">{props.detail.text}</div>
+                </div>
+            )
+    }
+    
 }
 
 const mapStateToProps = (state, ownProps) => {
 	const id = ownProps.match.params.id;
 	return {
         profile: state.firebase.profile,
-		project: state.firestore.data.projects && state.firestore.data.projects[id],
+        project: state.firestore.data.projects && state.firestore.data.projects[id],
+        projectDetails: state.firestore.ordered.projectDetails
 	};
 };
 
 export default compose(
     connect(mapStateToProps),
     firestoreConnect(props => {
+        let pid = props.match.params.id;
          return [
-               { collection: "projects", doc: props.match.params.id },
+            { collection: "projectDetails" },
+            { collection: "projects", doc: pid }
           ]
         }
     )
