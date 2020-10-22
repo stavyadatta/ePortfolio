4d45c5d5fc35859c4c96febdbb9f79b8edf3ce07
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-10-22 16:40:14 +1100

    Add palette feature to project details

--- Front-End/everest/src/Project_Details_Page.js
+++ Front-End/everest/src/Project_Details_Page.js
@@ -6,9 +6,16 @@ import { connect } from "react-redux";
 import "./Project_Details_Page.css";
 import defaultProjectImage from "./Images/project_image.jpg";
 
+const defaultPalette = {
+  primary:"#082F4E",
+  secondary: "#FFFFFF",
+  detail:"#a5a5a5"
+}
+
 function ProjectDetailsPage(props) {
     let project = props.project;
     let details = props.projectDetails;
+    
 
     //check if data is loaded
     if (!project || !details) {
@@ -24,33 +31,38 @@ function ProjectDetailsPage(props) {
         postDate.getFullYear();
 
     let imageUrl = project.imgUrl ? project.imgUrl : defaultProjectImage;
+    let palette = project.colourPalette?project.colourPalette:defaultPalette;
+    
+    let headerStyle = {background:palette.primary, color:palette.secondary}
+    let dateStyle = {color:palette.detail}
+    let descriptionStyle = {background:palette.secondary, color:palette.detail}
+    let detailStyle0 = {background:palette.detail, color:palette.secondary}
+    let detailStyle1 = {background:palette.secondary, color:palette.detail}
 
     return (
         <div className="projectLayout">
-            <div className="projectHeader">
+            <div className="projectHeader" style={headerStyle}>
                 <div className="detailImageWrap">
                     <img className="detailImage" alt="" src={imageUrl} />
                 </div>
                 <div className="projectTitle">
                     {project.projectName}
                     <div className="projectAuthor">{project.authorName}</div>
-                    <div className="projectDate">{dateString}</div>
+                    <div className="projectDate" style={dateStyle}>{dateString}</div>
                 </div>
             </div>
-            <div className="projectDescription">
-                <div className="descriptionTitle">Description</div>
-                <div className="descriptionBody">{project.projectDesc}</div>
+            <div className="projectDescription" style={descriptionStyle}>
+                <div className="descriptionTitle" style={descriptionStyle}>Description</div>
+                <div className="descriptionBody" style={descriptionStyle}>{project.projectDesc}</div>
             </div>
-            <ProjectDetailList details={details} />
+            <ProjectDetailList details={details} style0={detailStyle0} style1={detailStyle1}/>
         </div>
     );
 }
 
 const ProjectDetailList = (props) => {
     return props.details.map((detail) => {
-        let background = detail.position % 2 === 0 ? "#a5a5a5" : "white";
-        let textColor = detail.position % 2 === 1 ? "#a5a5a5" : "white";
-        let style = { background: background, color: textColor };
+        let style = detail.position % 2 === 0 ? props.style0 : props.style1;
         return (
             <ProjectDetail
                 key={detail.id}
