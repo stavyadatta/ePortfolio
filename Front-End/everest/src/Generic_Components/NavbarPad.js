73e5bc26fbf2242c33f028e70fd4d4cc07897ef1
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-11-18 18:03:42 +1100

    Add padding to project detail page and project edit page

---
+++ Front-End/everest/src/Generic_Components/NavbarPad.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavbarPad = (props) => {
+  return(
+    <div style={{
+      backgroundColor:props.color, 
+      width:"100%", 
+      top:0, 
+      height:"50px"}}>
+
+    </div>
+  )
+}
+
+export default NavbarPad;
