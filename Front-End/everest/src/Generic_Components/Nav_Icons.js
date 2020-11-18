b8d471a9310c3da256a3af97bcf46cab86d14079
Stavya Datta <stavyad@student.unimelb.edu.au>
2020-11-18 22:49:50 +0530

    Added logout functionality to myAccounts page

--- Front-End/everest/src/Generic_Components/Nav_Icons.js
+++ Front-End/everest/src/Generic_Components/Nav_Icons.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Nav_Icons.css";
 import Home from "../Icons/home_btn.svg";
 import About from "../Icons/about_btn.svg";
 import Signout from "../Icons/signout_btn.svg";
+import ConfirmDialog from "../Generic_Components/Dialog_Confirmation_Box";
+import firebase from "../Firebase";
+
 
 function NavIcons() {
+    const [confirmation, setConfirmation] = useState(false);
+    const handleLogout = function(){ 
+        firebase.auth().signOut()
+      }
     return(
         <div className = "nav_btns">
             <img src = {Home} id = "home_icon" alt = "home"/>
             <img src = {About} id = "about_icon" alt = "about"/>
-            <img src = {Signout} id = "signout_icon" alt = "signout"/>
+            <img src = {Signout} id = "signout_icon" alt = "signout" onClick={() => setConfirmation(true)}/>
+            <ConfirmDialog
+                title="Log Out"
+                open={confirmation}
+                setOpen={setConfirmation}
+                onConfirm={handleLogout}>
+                    Are you sure you want to Log Out?
+            </ConfirmDialog>
         </div>
     );
 }
