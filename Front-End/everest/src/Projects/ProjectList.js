89be7e0e754ab6a95988d95a6d6955a8feb38978
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-11-18 12:49:39 +1100

    Enable template switching

--- Front-End/everest/src/Projects/ProjectList.js
+++ Front-End/everest/src/Projects/ProjectList.js
@@ -1,175 +1,30 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-// import "./ProjectList.css";
-import "./ProjectList_Casual.css";
-import {ReactComponent as Plus}  from "../Icons/add_circle_outline-24px.svg";
-import Project from "../Generic_Components/Project";
-import { firestoreConnect } from "react-redux-firebase";
-import { compose } from "redux";
-import { connect } from "react-redux";
+import React from "react";
+import { Route } from "react-router-dom"
+import { connect } from "react-redux"
+import Casual from "./ProjectList_Casual";
+import Professional from "./ProjectList_Professional";
 
-function ProjectList(props) {
+const ProjectList = (props) => {
+  if(!props.profile){
+    return(<div>Loading...</div>)
+  }
 
-    const [colour, setColour] = useState("");
-    
-    const projects = props.projects;
+  console.log({Casual});
 
-    //placeholder
-    if(!projects){
-        console.log("loading");
-        return (<div>Loading...</div>)
-    }
-
-
-
-   function createProject(project) {
-        if (!project.imgURL) {
-            project.imgURL = "https://www.virvelle.com/wp-content/uploads/2018/12/project-management.jpg";
-        }
-        return (
-            <Project 
-                key={project.id}
-                id={project.id}
-                name={project.projectName}
-                description={project.projectDesc}
-                image={project.imgURL}
-            />
-        );
-    }
-
-
-    /*******************************************************  COMPONENTS OF THIS PAGE DEFINED BELOW  *****************************************************/
-
-
-    function getSelectedColour() {
-
-        setColour(document.getElementById("colourPalette").value);
-
-    }
-
-    function ThemeColour() {
-
-        return (
-
-            <div className="themeColour">
-                <form>
-                    Select Theme Colour:
-                    <select id="colourPalette" style={{width:"200px"}}>
-                        {/* <option value="0">Select Theme Colour:</option> */}
-                        <option value="default">Default</option>
-                        <option value="greyBlue">Grey-blue</option>
-                        <option value="lightBlue">Light-blue</option>
-                        <option value="greenishBlue">Greenish-blue</option>
-                    </select>
-                </form>
-
-                <button type="button" onClick={getSelectedColour}>Apply Colour</button>
-            </div>
-   
-        );
-    }
-
-
-    function Header(props) {
-
-        return (
-            <div id = "project_list_header" style={{ backgroundColor: colour==="greyBlue" ? "#426077" : colour==="lightBlue" ? "#51adcf" : colour==="greenishBlue" ? "#16697a" : "" }}>
-    
-                <h2 id = "project_list_header_title">{props.name}</h2>       
-                         
-            </div>
-        );
-      }
-      
-    
-    function AddProjectsButton() {
-        return (
-
-            <Link to="/form">
-
-                <div className="addProject">
-
-                    <Plus className="addProject-icon"/>
-
-                    <button className="addProjectText"> Add Project </button> 
-
-                </div>
-
-            </Link>       
-        );
-                
-            
-      }
-    
-    function GoBackButton() {
-        return (
-
-            <Link to="/profile">  
-
-                <div className="goBack">                  
-                
-                    <i className="far fa-arrow-alt-circle-left" />
-                
-                    <button className="goBackText"> Go Back </button>
-
-                </div>
-
-            </Link>      
-        );
-    }
-
-
-    /***********************************************************************************************************************************************************/
-
-
-    
-    return (
-
-        <div className="page_container" > 
-            
-            <Header name={"Projects"} />
-        
-            {/* <div className="projectListButtons"> */}
-
-                
-            <AddProjectsButton />
-
-            <GoBackButton />
-
-            {/* <ThemeColour /> */}
-            
-            {/* </div> */}
-
-            <div className="projects"> 
-
-                {projects.map(createProject)}    {/* PROJECTS GETTING RENDERED HERE */}
-                 
-            </div>
-
-            
-    
-        </div>
-    
-    
-  );
+  switch(props.profile.template){
+    case "Casual":
+      return <Route path="/projects/:userId"  component={Casual} />;
+    case "Professional":
+      return <Route path="/projects/:userId" component={Professional} />;
+    default:
+      return <Route path="/projects/:userId" component={Professional} />;
+  }
 }
 
-
 const mapStateToProps = (state, ownProps) => {
-    return {
-        projects:state.firestore.ordered.projects 
-    };
+  return {
+      profile:state.firebase.profile
+  };
 };
 
-export default compose(
-    connect(mapStateToProps),
-    firestoreConnect((props) => {
-        let userId = props.match.params.userId;
-        return [
-            {
-                collection: "projects",
-                where:[["userId", "==", userId]]
-            }
-        ];
-    })
-)(ProjectList);
+export default connect(mapStateToProps)(ProjectList);
---
+++ Front-End/everest/src/Projects/ProjectList_Casual.js
@@ -0,0 +1,142 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import "./ProjectList.css";
+import "./ProjectList_Casual.css";
+import {ReactComponent as Plus}  from "../Icons/add_circle_outline-24px.svg";
+import Project from "../Generic_Components/Project";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
+import { connect } from "react-redux";
+
+function ProjectList_Casual(props) {    
+    const projects = props.projects;
+
+    //placeholder
+    if(!projects){
+        console.log("loading");
+        return (<div>Loading...</div>)
+    }
+
+   function createProject(project) {
+        if (!project.imgURL) {
+            project.imgURL = "https://www.virvelle.com/wp-content/uploads/2018/12/project-management.jpg";
+        }
+        return (
+            <Project 
+                key={project.id}
+                id={project.id}
+                name={project.projectName}
+                description={project.projectDesc}
+                image={project.imgURL}
+            />
+        );
+    }
+
+
+    /*******************************************************  COMPONENTS OF THIS PAGE DEFINED BELOW  *****************************************************/
+
+
+
+    function Header(props) {
+
+        return (
+            <div id = "project_list_header">
+    
+                <h2 id = "project_list_header_title">{props.name}</h2>       
+                         
+            </div>
+        );
+      }
+      
+    
+    function AddProjectsButton() {
+        return (
+
+            <Link to="/form">
+
+                <div className="addProject">
+
+                    <Plus className="addProject-icon"/>
+
+                    <button className="addProjectText"> Add Project </button> 
+
+                </div>
+
+            </Link>       
+        );
+                
+            
+      }
+    
+    function GoBackButton() {
+        return (
+
+            <Link to="/profile">  
+
+                <div className="goBack">                  
+                
+                    <i className="far fa-arrow-alt-circle-left" />
+                
+                    <button className="goBackText"> Go Back </button>
+
+                </div>
+
+            </Link>      
+        );
+    }
+
+
+    /***********************************************************************************************************************************************************/
+
+
+    
+    return (
+
+        <div className="page_container" > 
+            
+            <Header name={"Projects"} />
+        
+            {/* <div className="projectListButtons"> */}
+
+                
+            <AddProjectsButton />
+
+            <GoBackButton />
+
+            {/* <ThemeColour /> */}
+            
+            {/* </div> */}
+
+            <div className="projects"> 
+
+                {projects.map(createProject)}    {/* PROJECTS GETTING RENDERED HERE */}
+                 
+            </div>
+
+            
+    
+        </div>
+    
+    
+  );
+}
+
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        projects:state.firestore.ordered.projects 
+    };
+};
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect((props) => {
+        let userId = props.match.params.userId;
+        return [
+            {
+                collection: "projects",
+                where:[["userId", "==", userId]]
+            }
+        ];
+    })
+)(ProjectList_Casual);
