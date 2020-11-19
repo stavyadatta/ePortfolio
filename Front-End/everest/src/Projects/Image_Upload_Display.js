7d786632445dec9dba84910ba0982e786c8063ab
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-11-19 13:12:44 +1100

    Fix image display

--- Front-End/everest/src/Projects/Image_Upload_Display.js
+++ Front-End/everest/src/Projects/Image_Upload_Display.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const ImageUploadDisplay = (props) => {
   return(
-    <div style={{height:"100%", width:"100%", objectFit: "cover"}}>
-      <label htmlFor={`imageUpload ${props.detailId}`}>
+    <div style={{maxHeight:"100%", maxWidth:"100%", display:"flex", justifyContent:"center", alignContent:"center"}}>
+      <label htmlFor={`imageUpload ${props.detailId}`} style={{display:"flex", justifyContent:"center", alignContent:"center"}}>
         <img className="detailImage" alt="" src={props.imageUrl}/>
       </label>
       <input id={`imageUpload ${props.detailId}`} type="file" accept="image/png, image/jpeg" style={{display:"none"}} onChange={props.handleChange}/>
