92a7c7f27bb59265d8faf07e6b687122f3a8a8a6
Joshua Robinson <jgrobinson@student.unimelb.edu.au>
2020-11-19 11:17:00 +1100

    Fix logged out navbar

--- Front-End/everest/src/Navbar/Navbar.js
+++ Front-End/everest/src/Navbar/Navbar.js
@@ -60,24 +60,26 @@ function LoggedInNavbar(props) {
 function LoggedOutNavbar() {
   return (
     <div className="Navbar">
-      <div className="navLogo">
-        <Link to="/">
-          <div className="navButton">logogohere</div>
-        </Link>
+      <div className="navBlock"></div>
+      <div className="navContent">
+        <div className="navLogo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="NavbarButtons"></div>
+        <div className="navUser">
+          <Link to="/register">
+            <div className="navButton" id="sign-up">
+              Sign-up
+            </div>
+          </Link>
+          <Link to="/login">
+            <div className="navButton" id="login">
+              Login
+            </div>
+          </Link>
+        </div>
       </div>
-      <div className="NavbarButtons"></div>
-      <div className="navUser">
-        <Link to="/login">
-          <div className="navButton" id="sign-up">
-            Sign-up
-          </div>
-        </Link>
-        <Link to="/login">
-          <div className="navButton" id="login">
-            Login
-          </div>
-        </Link>
-      </div>
+      
     </div>
   );
 }
