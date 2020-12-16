89e991e84972520a4d492d1c8dba2cc1996c59f2
NiphanSethi <58156889+NiphanSethi@users.noreply.github.com>
2020-12-16 18:05:17 +0700

    Homepage responsiveness done

--- Front-End/everest/src/Home_Page.js
+++ Front-End/everest/src/Home_Page.js
@@ -34,7 +34,6 @@ function FirstSegment() {
         <div className = "homepage_first_segment" id = "segment_one_home">
                 <div className="background_graphics">
                     <img src={Homepage_Image} id="homepage_image" alt="homepage" />
-                  {/* <img src = {Logo} id = "homepage_logo" alt = "Everest" /> */}
                 </div>
                 <div className="page_headers">
                 <p>Welcome to Everest!</p>
